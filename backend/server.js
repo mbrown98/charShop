9c414fba6550a1e65c691944682f3b5e2a545241
Matt Brown <matthewbrown0798@gmail.com>
2021-01-13 12:44:10 -0500

    user auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,22 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 // Routes
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
 app.use("/api/products", productRoutes);
+
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
