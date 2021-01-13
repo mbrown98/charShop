e2b944abc059fe5c43dc8ab050df5efcdecb0a79
Matt Brown <matthewbrown0798@gmail.com>
2021-01-12 19:31:17 -0500

    remove item from cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   console.log({ id });
@@ -17,5 +17,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     },
   });
 
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  console.log("rem id", id);
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
