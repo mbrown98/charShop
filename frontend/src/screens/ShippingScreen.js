36b54fbccf46892f00ccc5762d0c36868ead53b7
Matt Brown <matthewbrown0798@gmail.com>
2021-01-13 20:10:06 -0500

    checkout steps

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -6,6 +6,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
 import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   console.log({ cart });
@@ -28,6 +29,7 @@ const ShippingScreen = ({ history }) => {
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
