c099c61659992248f197af66ae3b540bb118ff1f
Matt Brown <matthewbrown0798@gmail.com>
2021-01-14 17:17:57 -0500

    created order details screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,5 +6,5 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
-router.route("/id").get(protect, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 export default router;
