5bb2aa243d84d88626abe299a1f6cc232b61a385
Matt Brown <matthewbrown0798@gmail.com>
2021-01-14 15:07:37 -0500

    new order route and controller

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // Routes
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
+import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 connectDB();
 
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 
 app.use("/api/users", userRoutes);
+
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
