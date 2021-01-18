5701b1f51535d8d89222acfc9e80555afc6736fd
Matt Brown <matthewbrown0798@gmail.com>
2021-01-18 11:36:20 -0500

    added home page search functionality"

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,15 @@ import Product from "../models/productModel.js";
 // @route  GET /api/products
 // @access  PUBLIC
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
@@ -20,7 +28,6 @@ const getProductById = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Product not found");
   }
-  res.json(product);
 });
 
 // @desc  Delete product by id
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,7 @@ function App() {
           <Route path="/admin/orderlist" component={OrderListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/" exact component={HomeScreen} />
         </Container>
       </main>
